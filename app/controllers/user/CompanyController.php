e2304fbc036f8c8c1faf2a81291920cfd4c66a26
Mokadev-vn <nhantu1001@gmail.com>
2020-12-10 08:25:55 +0700

    code user

--- app/controllers/user/CompanyController.php
+++ app/controllers/user/CompanyController.php
@@ -6,9 +6,9 @@ use App\Models\User;
 class CompanyController extends Controller{
 
     public function profile(){
-        echo 'aaaaa';
+        return $this->view('user.profile-company');
     }
 
-    
+
 
 }
